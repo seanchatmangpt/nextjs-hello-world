62ef5565100c68bbfbd43e3177d4dd1c10ce6798
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2023-08-29 08:57:23 -0700

    Back to square one.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      }
-    ],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
